e5e625db9b2979ecf76c76ccc12d5d040fe53215
Chrystian Cortez <cecortez144@gmail.com>
2019-09-28 17:02:47 -0600

    Updates

---
+++ precache-manifest.030ec04373f2a943d8f73f76143aeac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a661fa04768822e322b111af97f9a4f",
+    "url": "/CECORTEZ144/paginaWebReact/index.html"
+  },
+  {
+    "revision": "7afa9c33c030f2d024f5",
+    "url": "/CECORTEZ144/paginaWebReact/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "faa34e326583b5e1ea66",
+    "url": "/CECORTEZ144/paginaWebReact/static/js/2.54bbda69.chunk.js"
+  },
+  {
+    "revision": "7afa9c33c030f2d024f5",
+    "url": "/CECORTEZ144/paginaWebReact/static/js/main.1f4b470f.chunk.js"
+  },
+  {
+    "revision": "7770c5421008c67e8879",
+    "url": "/CECORTEZ144/paginaWebReact/static/js/runtime-main.657fcb14.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/CECORTEZ144/paginaWebReact/static/media/logo.25bf045c.svg"
+  }
+]);
